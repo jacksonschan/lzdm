13cd1fa4b959634724ef86d8e4a338c6fcc7b627
jacksonschan <jackson.chan15@gmail.com>
2020-08-04 12:30:01 -0700

    fixing rgdal crashing due to local installation

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -15,7 +15,6 @@
 
 ## Dependencies ----
 ## Add one line by package you want to add as dependency
-usethis::use_package( "thinkr" )
 usethis::use_package( "shiny" )
 usethis::use_package( "dplyr" )
 usethis::use_package( "ggplot2" )
